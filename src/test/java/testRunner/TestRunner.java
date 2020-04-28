5266e68541ac2ce99e26c1b4a18791f44f4b4c1a
ahmedwaquar29 <theahmed0429@gmail.com>
2020-04-28 11:34:46 +0530

    upload

---
+++ src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,16 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Feature"
+		, glue = "stepDefinition"
+		, plugin = { "json:target/cucumber.json" }
+		)
+public class TestRunner {
+
+}
